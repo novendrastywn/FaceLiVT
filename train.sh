e9f5ee3e32110deaa46a8d8a998d318aa7d02d01
novendrastywn <31612686+novendrastywn@users.noreply.github.com>
2026-02-24 16:01:11 +0800

    train.sh

--- train.sh
+++ train.sh
@@ -8,5 +8,5 @@ MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 NUM_GPU=1
 
 torchrun --master_addr=$MASTER_ADDR --nproc_per_node=$NUM_GPU --master_port=$PORT train_v2_aux_distil.py \
-configs/glint360k_facelivtv2_l_restart.py #ms1mv3_facelivtv2_s.py #glint360k_facelivtv2_s.py
+configs/glint360k_facelivtv2_l.py #ms1mv3_facelivtv2_s.py #glint360k_facelivtv2_s.py
 
